8fb30a9eb112309e7f4e16a7e6c7ed101ca15d45
xunjianyin <xjyin@pku.edu.cn>
2025-08-25 00:15:33 -0400

    Update repository configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/maze-generator-solver/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/Maze-Generator-And-Solver/' : '/',
   server: {
     port: 3000,
   },
